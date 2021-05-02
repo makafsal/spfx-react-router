50cf2624890ad6ef84a0004da3a038a1f67b1c96
Afsal K <afsal.k@global-infonet.com>
2021-05-02 11:47:23 +0530

    Initialize...

---
+++ src/webparts/dasboard/loc/mystrings.d.ts
@@ -0,0 +1,10 @@
+declare interface IDasboardWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+}
+
+declare module 'DasboardWebPartStrings' {
+  const strings: IDasboardWebPartStrings;
+  export = strings;
+}
